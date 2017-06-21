07f1d8a01ea317a0922826f47ea280e45e9fa916
Rafael <rafaeddy@gmail.com>
2017-06-21 14:41:46 -0300

    estrutura

---
+++ CONTROLLER/connection.js
@@ -0,0 +1,13 @@
+// requer conexao ao mysql
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "123456789"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
--- app.js
+++ app.js
@@ -1,67 +1,105 @@
+// framework express
 var express = require('express');
+// modulo body-parser, middleware para analisar o JSON
 var bodyParser = require('body-parser');
 
+// constantes de endereços URL
+const urlClientes = "cliente"; 
+const urlVendas   = "venda";
+const urlFilmes   = "filme";
+
+// cria o express e usa o parser como json
 var app = express();
 app.use(bodyParser.json());
-
-var tasks = {};
-var sequenceId = 0;
 
 // controle do 404
 function notFound(res){
     res.status(404).send('Página não existe!');
 }
 
+// escuta a porta 8089 
 app.listen(8089 ,function(){
-    console.log('ola express');
+    console.log('Trabalho final DM104 - INATEL');
 });
 
-// acessando o / retorna tudo
+// acessando o / retorna um status , poderia ser o WSDL
 app.get('/',function(req , res){
-    res.json(tasks);
+    res.status(200).send('Trabalho final DM104 - INATEL');
 });
 
-// GET 
-app.get('/:id' , function(req, res){
-    var task = tasks[req.params.id];
-    if (task){
-        res.status(200).json(task);
-    }else {
-        notFound(res);
-    }
+
+// --- funcoes do / --- //
+app.get('/' + urlClientes ,function(req , res){
+    retorna cliente
 });
 
-// POST 
-app.post('/' , function(req, res){
-    var newTask = {
-        id: ++sequenceId,
-        done: req.body.done,
-        description: req.body.description
-    };
-
-    tasks[sequenceId] = newTask;
-    res.status(200).json(newTask);
+app.get('/' + urlVendas ,function(req , res){
+    retorna vendas
 });
 
-// PUT
-app.put('/:id' , function(req, res){
-    var task = tasks[req.params.id];
-    if (task){
-        task.done = req.body.done;
-        task.description = req.body.description;
-        res.status(200).json(task);
-    }else{
-        notFound(res);
-    }   
+app.get('/' + urlFilmes ,function(req , res){
+    retorna vendas
 });
 
-// DELETE
-app.delete('/:id' , function(req, res){
-    var task = tasks[req.params.id];
-    if (task){
-        delete tasks[req.params.id];
-        res.status(200).send('Apagou!');
-    }else{
-        notFound(res);
-    }
+
+
+// --- funcoes do GET por id --- //
+app.get('/'+urlVendas+'/:id' , function(req, res){
+    
+    retorna venda em especifico
+});
+
+app.get('/'+urlFilmes+'/:id' , function(req, res){
+    
+    retorna filme em especifico
+});
+
+app.get('/'+urlClientes+'/:id' , function(req, res){
+    
+    retorna cliente em especifico
+});
+
+
+
+// --- funcoes do POST --- //
+app.post('/' + urlVendas , function(req, res){    
+     faz o post da venda com itens
+});
+
+app.post('/' + urlFilmes , function(req, res){    
+     faz o post de filme
+});
+
+app.post('/' + urlClientes , function(req, res){    
+     faz o post do cliente
+});
+
+
+
+// --- funcoes do PUT --- //
+app.put('/'+urlVendas+'/:id' , function(req, res){
+    // atualiza venda com itens
+});
+
+app.put('/'+urlClientes+'/:id' , function(req, res){
+    // atualiza cliente
+});
+
+app.put('/'+urlFilmes+'/:id' , function(req, res){
+    // atualiza filme
+});
+
+
+
+// --- funcoes do DELETE --- //
+app.delete('/'+urlVendas+'/:id' , function(req, res){
+    deleta a venda com itens
+});
+
+app.delete('/'+urlFilmes+'/:id' , function(req, res){
+    deleta o filme
+});
+
+app.delete('/'+urlClientes+'/:id' , function(req, res){
+    deleta o cliente
 });
