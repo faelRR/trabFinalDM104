8f5dd3579d806e168704db7e60d842b1680ca3f4
Rafael <rafaeddy@gmail.com>
2017-06-26 13:50:57 -0300

    CRUD completo e testado

--- CONTROLLER/connection.js
+++ CONTROLLER/connection.js
@@ -4,10 +4,21 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "123456789"
+  password: "",
+  port: 3306,
+  database: "dm104"
 });
 
+// deixa publico a conexao
+exports.con = con;
+
+// faz o teste da conexao
 con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
+
+  // se obteve erro ja finaliza
+  if (err) throw "Falha ao conectar ao banco de dados ==> " + err;
+  
+  // se conectou correto
+  console.log("Banco de dados conectado com sucesso!");
+  
 });
